aac3ab02a8c84554ed7f650c922c7054d293939e
TW G <twg@TWui-Macmini.local>
2023-02-08 06:51:26 +0900

    테스트

---
+++ src/navigations/index.js
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './AuthStack';
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
